35c93daa16869b0764b68cf6135a1e6d6f085893
tony_shen <bonashen@163.com>
2016-12-28 16:05:02 +0800

    modified aspect method

--- example/node/flow-test.js
+++ example/node/flow-test.js
@@ -10,31 +10,13 @@ var printEventStack = function (event) {
     console.log("event flow :", event.flow.getEventIdsPath());
 };
 
-ebus.before("emit", function (handler) {
-    console.log("before emit=>", handler);
-    // console.log("early event: ", this.getEarlyEvent() ? this.getEarlyEvent().id : undefined);
-    console.log("closest event path: ", this.getCurrentEvent() ? this.getCurrentEvent().flow.getEventIdsPath() : undefined);
-    handler.remove();
-});
-
-ebus.after("beforeEmit", function (handler, result) {
-    console.log("after beforeEmit=>", handler);
-    console.log("current event path: ", this.getCurrentEvent() ? this.getCurrentEvent().flow.getEventIdsPath() : undefined);
-}).remove();
-
-ebus.before("beforeEmit", function (handler) {
-    // console.log(handler);
-    console.log("beforeEmit,current event path: ", this.getCurrentEvent() ? this.getCurrentEvent().flow.getEventIdsPath() : undefined);
-    handler.remove();
-});
-
 ebus.on("start", function (event) {
     console.log("The game is start...");
 })
     .on("chase", function (event) {
         console.log("overtaken");
         printEventStack(event);
-        ebus.emit("overtaken");
+        ebus("overtaken");
     })
     .flow(
         {from: "ready", to: "start"},
